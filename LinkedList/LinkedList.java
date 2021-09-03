f6565e0250050472f583f5c5b9d91a86b5ad0166
touhidpasha <touhidpasha552@gmail.com>
2021-09-03 21:51:33 +0530

    linked all the nodes

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,5 +1,42 @@
 package LinkedList;
 
-public class LinkedList {
+public class LinkedList<K>{
+	public INode<K> head;
+	public INode<K> tail;
+	
+	public LinkedList() {
+		this.head = null;
+		this.tail = null;	
+	}
+	
+	public void add(INode<K> newNode) {
+		if(this.tail == null) {
+			this.tail = newNode;
+		}
+		if(this.head == null) {
+			this.head = newNode;
+		}
+		else {
+			INode<K>tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+	
+	public void append(INode<K> newNode) {
+		if(this.head == null) {
+			this.head = newNode;
+		}
+		if(this.tail == null) {
+			this.tail = newNode;
+		}
+		else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
 
+		}
+	}
+	public void printNodes() {
+		System.out.println("My Nodes: "+head);
+	}
 }
--- LinkedList/MainClass.java
+++ LinkedList/MainClass.java
@@ -11,11 +11,25 @@ public class MainClass {
 		
 		myFirstNode.setNext(mySecondNode);
 		mySecondNode.setNext(myThirdNode);
+	
 		
+	}
+	public static void link() {//this function will link 56->30->70
+		Node<Integer> myFirstNode = new Node<>(56);
+		Node<Integer> mySecondNode = new Node<>(30);
+		Node<Integer> myThirdNode = new Node<>(70);
+		
+		LinkedList<Integer> myLinkedList = new LinkedList<Integer>();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
+		myLinkedList.add(myThirdNode);
+		myLinkedList.printNodes();
 	}
 	
+	
 	public static void main(String [] args) {
-		Create();
-		
+		Create();//Create() method to create a linked list 
+		link();//this function will link 56->30->70
 	}
+	
 }
